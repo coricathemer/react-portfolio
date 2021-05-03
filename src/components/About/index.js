9d68a67aca1fa187ec3066926f53ab22ecc31638
coricathemer <coricathemer@gmail.com>
2021-05-03 15:11:29 -0700

    updated nav and css color scheme and fonts

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -4,7 +4,7 @@ import coverImage from "../../assets/cover/cover-image.jpg"
 function About() {
   return (
     <section className="my-5">
-      <h1 id="about">Cori Cathemer</h1>
+      <h1 id="about">Full Stack Developer</h1>
       <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
     </section>
   );
